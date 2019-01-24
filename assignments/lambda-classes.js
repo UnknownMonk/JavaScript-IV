d7cfd1bc91c392d70ab3114887c513fa691901d4
UnknownMonk <smodcartoon@gmail.com>
2019-01-24 15:02:25 -0500

    Added person class to lambda classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,13 @@
 // CODE here for your Lambda Classes
+class Person {
+  constructor(personAttr) {
+    this.name = personAttr.name;
+    this.age = personAttr.age;
+    this.location = personAttr.location;
+    this.gender = personAttr.gender;
+  }
+
+  speak() {
+    return `Hello my name is ${this.name}, I am from ${this.location}.`;
+  }
+}
